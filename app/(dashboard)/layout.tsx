aa59007d5a95e6d3d4b6b9e671f01427b9acf932
Deák Bálint <deakbalint@office42.hu>
2025-01-11 12:06:32 +0100

    workflows init

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,20 +1,19 @@
-import { SignedIn, UserButton } from '@clerk/nextjs'
-import { Separator } from '@radix-ui/react-separator'
-import React from 'react'
-import BreadcrumbHeader from '../../components/BreadcrumbHeader'
-import DesktopSidebar from '../../components/Sidebar'
-import { ModeToggle } from '../../components/ThemeModeToggle'
+import { SignedIn, UserButton } from '@clerk/nextjs';
+
+import React from 'react';
+import BreadcrumbHeader from '../../components/BreadcrumbHeader';
+import DesktopSidebar from '../../components/Sidebar';
+import { ModeToggle } from '../../components/ThemeModeToggle';
+import { Separator } from '../../components/ui/separator';
 
 export default function layout({ children }: { children: React.ReactNode }) {
-
-
 	return (
 		<div className='flex h-screen'>
 			<DesktopSidebar />
-			<div className="flex flex-col flex-1 min-h-screen">
-				<header className="flex items-center justify-between px-6 py-4 h-[50px] container">
+			<div className='flex min-h-screen flex-1 flex-col'>
+				<header className='container flex h-[50px] items-center justify-between px-6 py-4'>
 					<BreadcrumbHeader />
-					<div className="flex items-center gap-1">
+					<div className='flex items-center gap-1'>
 						<ModeToggle />
 						<SignedIn>
 							<UserButton />
@@ -22,12 +21,12 @@ export default function layout({ children }: { children: React.ReactNode }) {
 					</div>
 				</header>
 				<Separator />
-				<div className="overflow-auto">
-					<div className="flex-1 container py-4 text-accent-foreground">
+				<div className='overflow-auto'>
+					<div className='container flex-1 px-6 py-4 text-accent-foreground'>
 						{children}
 					</div>
 				</div>
 			</div>
 		</div>
-	)
+	);
 }
