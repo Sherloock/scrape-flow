0f12572ce2daf1b4edee257677982a7d77c8b69c
Deák Bálint <deakbalint@office42.hu>
2025-01-11 12:41:29 +0100

    wf init

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,25 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
 
 export default clerkMiddleware(async (auth, request) => {
-// TODO: Fix this middleware somehow that sucks CLERK!!
-  try {
-	 console.log('Authentication Status', auth())
-    if (!isPublicRoute(request)) {
-      await auth().protect()
-    }
-  } catch (error) {
-    console.error('Authentication error:', error)
-  }
-})
+	try {
+		const authStatus = await auth();
+		console.log('Authentication Status:', authStatus);
+
+		if (!isPublicRoute(request)) {
+			await authStatus.protect();
+		}
+	} catch (error) {
+		console.error('Authentication error:', error);
+	}
+});
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-}
+	matcher: [
+		// Skip Next.js internals and all static files, unless found in search params
+		'/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+		// Always run for API routes
+		'/(api|trpc)(.*)',
+	],
+};
