09a0a4d456a9302ff6460823ff0b98e872459f55
Deák Bálint <deakbalint@office42.hu>
2025-01-14 22:12:19 +0100

    node header

--- app/(dashboard)/workflows/page.tsx
+++ app/(dashboard)/workflows/page.tsx
@@ -8,16 +8,16 @@ import WorkflowCard from "./_components/WorkflowCard";
 
 function page() {
 	return (
-		<div className="flex h-full flex-1 flex-col">
-			<div className="flex justify-between">
-				<div className="flex flex-col">
-					<h1 className="text-3xl font-bold">Workflows</h1>
-					<p className="text-muted-foreground">Manage your workflows</p>
+		<div className='flex h-full flex-1 flex-col'>
+			<div className='flex justify-between'>
+				<div className='flex flex-col'>
+					<h1 className='text-3xl font-bold'>Workflows</h1>
+					<p className='text-muted-foreground'>Manage your workflows</p>
 				</div>
 				<CreateWorkflowDialog />
 			</div>
 
-			<div className="h-full py-6">
+			<div className='h-full py-6'>
 				<Suspense fallback={<UserWorkFlowsSkeleton />}>
 					<UserWorkFlows />
 				</Suspense>
@@ -27,9 +27,9 @@ function page() {
 }
 function UserWorkFlowsSkeleton() {
 	return (
-		<div className="space-y-2">
+		<div className='space-y-2'>
 			{[1, 2, 3, 4].map((index) => (
-				<Skeleton key={index} className="h-32 w-full" />
+				<Skeleton key={index} className='h-32 w-full' />
 			))}
 		</div>
 	);
@@ -40,8 +40,8 @@ async function UserWorkFlows() {
 
 	if (!workflows) {
 		return (
-			<Alert variant="destructive">
-				<AlertCircle className="h-4 w-4" />
+			<Alert variant='destructive'>
+				<AlertCircle className='h-4 w-4' />
 				<AlertTitle>Error</AlertTitle>
 				<AlertDescription>
 					Something went wrong while fetching your workflows. Please try again
@@ -53,23 +53,23 @@ async function UserWorkFlows() {
 
 	if (workflows.length === 0) {
 		return (
-			<div className="flex-center h-full flex-col gap-4">
-				<div className="flex-center size-20 rounded-full bg-accent">
-					<InboxIcon className="size-10 stroke-primary" />
+			<div className='flex-center h-full flex-col gap-4'>
+				<div className='flex-center size-20 rounded-full bg-accent'>
+					<InboxIcon className='size-10 stroke-primary' />
 				</div>
-				<div className="flex flex-col gap-1 text-center">
-					<p className="font-bold">No workflow created yet</p>
-					<p className="text-sm text-muted-foreground">
+				<div className='flex flex-col gap-1 text-center'>
+					<p className='font-bold'>No workflow created yet</p>
+					<p className='text-sm text-muted-foreground'>
 						Create your first workflow to get started.
 					</p>
 				</div>
-				<CreateWorkflowDialog triggerText="Create your first workflow" />
+				<CreateWorkflowDialog triggerText='Create your first workflow' />
 			</div>
 		);
 	}
 
 	return (
-		<div className="grid grid-cols-1 gap-4">
+		<div className='grid grid-cols-1 gap-4'>
 			{workflows.map((workflow) => (
 				<WorkflowCard key={workflow.id} workflow={workflow} />
 			))}
