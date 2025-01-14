fa888be27491e038947d0cb2bde987991e47b575
Deák Bálint <deakbalint@office42.hu>
2025-01-14 21:52:30 +0100

    format

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -6,26 +6,26 @@ import { SignedIn, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex h-screen">
-      <DesktopSidebar />
-      <div className="flex min-h-screen flex-1 flex-col">
-        <header className="container flex h-[50px] items-center justify-between px-6 py-4">
-          <BreadcrumbHeader />
-          <div className="flex items-center gap-1">
-            <ModeToggle />
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </div>
-        </header>
-        <Separator />
-        <div className="overflow-auto">
-          <div className="container flex-1 px-6 py-4 text-accent-foreground">
-            {children}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='flex h-screen'>
+			<DesktopSidebar />
+			<div className='flex min-h-screen flex-1 flex-col'>
+				<header className='container flex h-[50px] items-center justify-between px-6 py-4'>
+					<BreadcrumbHeader />
+					<div className='flex items-center gap-1'>
+						<ModeToggle />
+						<SignedIn>
+							<UserButton />
+						</SignedIn>
+					</div>
+				</header>
+				<Separator />
+				<div className='overflow-auto'>
+					<div className='container flex-1 px-6 py-4 text-accent-foreground'>
+						{children}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
