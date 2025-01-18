75a23eb9101bc6c4c5725c05acfda769c4d82b7f
Deák Bálint <deakbalint@office42.hu>
2025-01-18 12:57:00 +0100

    aside + dd

--- app/workflow/_components/Editor.tsx
+++ app/workflow/_components/Editor.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import FlowEditor from "@/app/workflow/_components/FlowEditor";
+import TaskMenu from "@/app/workflow/_components/TaskMenu";
 import Topbar from "@/app/workflow/_components/topbar/Topbar";
 import { Workflow } from "@prisma/client";
 import { ReactFlowProvider } from "@xyflow/react";
@@ -13,6 +14,7 @@ function Editor({ workflow }: { workflow: Workflow }) {
 
 				<Topbar title={'Workflow Editor'} subtitle={workflow.name} workflowId={workflow.id} />
 				<section className="flex h-full overflow-auto">
+					<TaskMenu />
 					<FlowEditor workflow={workflow} />
 				</section>
 			</div>
---
+++ app/workflow/_components/TaskMenu.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import { TaskRegistry } from "@/lib/workflow/task/Registry";
+import { TaskType } from "@/types/task";
+import React from "react";
+
+function TaskMenu() {
+	return (
+		<aside className='h-full w-[340px] min-w-[340px] max-w-[340px] border-separate overflow-auto border-r-2 p-2 px-4'>
+			<Accordion
+				type='multiple'
+				className='w-full'
+				defaultValue={["extraction"]}
+			>
+				<AccordionItem value='extraction'>
+					<AccordionTrigger className='font-bold'>
+						Data Extraction
+					</AccordionTrigger>
+					<AccordionContent>
+						<TaskMenuBtn taskType={TaskType.PAGE_TO_HTML} />
+					</AccordionContent>
+				</AccordionItem>
+			</Accordion>
+		</aside>
+	);
+}
+
+export default TaskMenu;
+
+function TaskMenuBtn({ taskType }: { taskType: TaskType }) {
+	const task = TaskRegistry[taskType];
+
+	const onDragStart = (e: React.DragEvent, taskType: TaskType) => {
+		e.dataTransfer.setData("application/reactflow", taskType);
+		e.dataTransfer.effectAllowed = "move";
+	};
+
+	return (
+		<Button
+			variant={"secondary"}
+			className='flex w-full items-center justify-between gap-2 border'
+			draggable
+			onDragStart={(e) => {
+				onDragStart(e, taskType);
+			}}
+		>
+			<div className='flex items-center gap-2'>
+				<task.icon size={20} />
+				{task.label}
+			</div>
+		</Button>
+	);
+}
