5315dde0fcd52af3156dfc8621210c37d951ce25
Deák Bálint <deakbalint@office42.hu>
2025-02-03 21:47:19 +0100

    run view

---
+++ app/workflow/runs/[workflowId]/[executionId]/page.tsx
@@ -0,0 +1,49 @@
+import { getWorkflowExecutionWithPhases } from "@/actions/workflows/getWorkflowExecutionWithPhases";
+import Topbar from "@/app/workflow/_components/topbar/Topbar";
+import ExecutionViewer from "@/app/workflow/runs/[workflowId]/[executionId]/_components/ExecutionViewer";
+import { Loader2Icon } from "lucide-react";
+import { Suspense } from "react";
+
+export default function ExecutionViewPage({
+	params,
+}: {
+	params: { workflowId: string; executionId: string };
+}) {
+	return (
+		<div className="flex h-screen w-full flex-col overflow-hidden">
+			<Topbar
+				workflowId={params.workflowId}
+				title="Workflow run details"
+				subtitle={`Run ID: ${params.executionId}`}
+				hideButtons={true}
+			></Topbar>
+			<section className="flex h-full overflow-auto">
+				<Suspense
+					fallback={
+						<div className="flex h-full w-full items-center justify-center">
+							<Loader2Icon
+								className="size-10 animate-spin stroke-primary"
+								size={20}
+							/>
+						</div>
+					}
+				>
+					<ExecutionViewerWrapper executionId={params.executionId} />
+				</Suspense>
+			</section>
+		</div>
+	);
+}
+
+async function ExecutionViewerWrapper({
+	executionId,
+}: {
+	executionId: string;
+}) {
+	const workflowExecution = await getWorkflowExecutionWithPhases(executionId);
+	if (!workflowExecution) {
+		return <div>Execution not found</div>;
+	}
+
+	return <ExecutionViewer initialData={workflowExecution} />;
+}
