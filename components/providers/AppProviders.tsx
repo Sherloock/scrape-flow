abbf1397d1e71ac2c9c1cedf0883623c78b485e9
Deák Bálint <deakbalint@office42.hu>
2025-02-13 20:26:35 +0100

    a

--- components/providers/AppProviders.tsx
+++ components/providers/AppProviders.tsx
@@ -4,10 +4,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "next-themes";
 import { useState } from "react";
+import NextTopLoader from "nextjs-toploader";
 export function AppProviders({ children }: { children: React.ReactNode }) {
 	const [queryClient] = useState(() => new QueryClient());
 	return (
 		<QueryClientProvider client={queryClient}>
+			<NextTopLoader color="#10b981" showSpinner={false} />
 			<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
 				{children}
 			</ThemeProvider>
