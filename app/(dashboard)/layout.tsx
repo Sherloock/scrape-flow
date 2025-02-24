0311522fcf056c042766425544700f90cebed640
Deák Bálint <deakbalint@office42.hu>
2025-02-24 17:19:00 +0100

    HEADER

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -9,19 +9,22 @@ export default function layout({ children }: { children: React.ReactNode }) {
 	return (
 		<div className="flex h-screen">
 			<DesktopSidebar />
-			<div className="flex min-h-screen flex-1 flex-col">
-				<header className="container flex h-[50px] items-center justify-between px-6 py-4">
-					<BreadcrumbHeader />
-					<div className="flex items-center gap-1">
-						<ModeToggle />
-						<SignedIn>
-							<UserButton />
-						</SignedIn>
-					</div>
-				</header>
+			<div className="flex min-h-screen flex-1 flex-col justify-center">
+				<div className="flex items-center justify-center">
+					<header className="container flex h-[50px] items-center justify-between px-6 py-4">
+						<BreadcrumbHeader />
+						<div className="flex items-center gap-1">
+							<ModeToggle />
+							<SignedIn>
+								<UserButton />
+							</SignedIn>
+						</div>
+					</header>
+				</div>
 				<Separator />
-				<div className="overflow-auto">
-					<div className="container flex-1 px-6 py-4 text-accent-foreground">
+
+				<div className="flex flex-1 justify-center overflow-auto">
+					<div className="container flex flex-1 items-center justify-center px-6 py-4 text-accent-foreground">
 						{children}
 					</div>
 				</div>
